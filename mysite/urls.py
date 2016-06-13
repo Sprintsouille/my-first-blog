72d98538d53a48dac7823660834ab543914dd468
Flavien <f.petitguyot@gmail.com>
2016-06-13 13:17:44 +0200

    Modification du HTML su site

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+
+from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)), # Cela signifie que pour chaque URL qui commence par admin/, Django affichera la vue correspondante.
+    url(r'', include('blog.urls')),
+    # Django va maintenant rediriger tout ce qui arrive sur "http://127.0.0.1:8000/" vers blog.urls puis regardera dans ce fichier pour y trouver la suite des instructions à suivre.
 ]
